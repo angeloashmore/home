2ec85cdd9c8e6698b84ab1d1042798a6eb5ee11f
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-08-23 12:03:55 -1000

    feat: add nightly neovim overlay

---
+++ overlays/neovim.nix
@@ -0,0 +1,11 @@
+self: super: {
+  neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (old: {
+    version = "master";
+    src = super.fetchFromGitHub {
+      owner = "neovim";
+      repo = "neovim";
+      rev = "8c49e3d50959e24dadd688f56a18f104bd5fd934";
+      sha256 = "1qqmfys8hm60j4hgw861yad2x94jadd7247jjgqsxl4ij123hgdg";
+    };
+  });
+}
